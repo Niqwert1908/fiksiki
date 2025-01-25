afba49a34156ee313d264daabd8a9ebdbd3f42f2
Nikita Beresnev <163751008+Niqwert1908@users.noreply.github.com>
2025-01-25 12:35:13 +0300

    Update pr1.cpp

--- pr1.cpp
+++ pr1.cpp
@@ -1,10 +1,21 @@
-#include <iostream>
-using namespace std;
-
-int main()
+Ex4_Icon ICON "myicon.ico"
+Ex4_Menu MENU
+POPUP "&File"
+MENUITEM "&Open...ItCtri-O", 2
+MENUITEM "&Save",
+MENUITEM "Save &As...
+-", 4
+MENUITEM SEPARATOR
+MENUITEM "&Hex view", 5, CHECKED GRAYED MENUITEM "&Exit\tAlt-F4", 6
+POPUP "&Edit"
+{ MENUITEM "&Copy",
+",7
+MENUITEM "&Paste", 8
+POPUP "Popup"
 {
-  cout<<"Hello"<<endl;
-  return 0;
-}
-
-
+MENUITEM "1", 9
+Пример
+MENUITEM "2", 10
+MENUITEM "3", 11 }
+MENUITEM SEPARATOR MENUITEM "Search", 12
+POPUP "&Help" { MENUITEM "&About...\tF1", 13} }
